00a21c1359894a14668d1775b493ace164a6cda5
caoyingjunz <cao.yingjunz@gmail.com>
2026-01-03 22:14:15 +0800

    放开镜像规格检查限制 (#233)

--- pkg/controller/rainbow/task.go
+++ pkg/controller/rainbow/task.go
@@ -42,15 +42,15 @@ func (s *ServerController) preCreateTask(ctx context.Context, req *types.CreateT
 	} else {
 		var errs []error
 		// TODO: 其他不合规检查
-		for _, image := range req.Images {
-			if strings.Contains(image, "\"") { // 分割镜像名称和版本
-				errs = append(errs, fmt.Errorf("invaild image(%s)", image))
-				parts := strings.Split(image, ":")
-				if len(parts) != 2 {
-					errs = append(errs, fmt.Errorf("invalid image format, should be 'name:tag' (%s)", image))
-				}
-			}
-		}
+		//for _, image := range req.Images {
+		//	if strings.Contains(image, "\"") { // 分割镜像名称和版本
+		//		errs = append(errs, fmt.Errorf("invaild image(%s)", image))
+		//		parts := strings.Split(image, ":")
+		//		if len(parts) != 2 {
+		//			errs = append(errs, fmt.Errorf("invalid image format, should be 'name:tag' (%s)", image))
+		//		}
+		//	}
+		//}
 		return utilerrors.NewAggregate(errs)
 	}
 
