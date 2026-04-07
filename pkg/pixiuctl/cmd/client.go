cdb373ffd1861c0205c8d24a61ca88815dd04140
puzhihao <pzh905655347@gmail.com>
2026-04-07 22:27:13 +0800

    fix pixiuctl limit

--- pkg/pixiuctl/cmd/client.go
+++ pkg/pixiuctl/cmd/client.go
@@ -134,7 +134,6 @@ func (pc *PixiuHubClient) ListRegistries() ([]model.Registry, error) {
 func (pc *PixiuHubClient) ListImages(page, pageSize int) ([]model.Image, error) {
 	var result ImageListResult
 	url := fmt.Sprintf("%s/api/v2/images?user_id=%s&page=%d&limit=%d", pc.baseURL, pc.userInfo.UserId, page, pageSize)
-	fmt.Println(url)
 	httpClient := util.HttpClientV2{URL: url}
 	if err := httpClient.Method("GET").
 		WithTimeout(5 * time.Second).
